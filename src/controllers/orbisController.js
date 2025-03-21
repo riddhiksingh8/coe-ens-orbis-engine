139d70fef10effb7d1d39f849ad08c335f6e983a
Riddhi K Singh <Riddhi.Singh@in.ey.com>
2025-03-21 17:06:52 +0530

    update

--- src/controllers/orbisController.js
+++ src/controllers/orbisController.js
@@ -1,9 +1,6 @@
 import { getOrbisData } from "../mock/orbis.js";
 import { updateInternalOrgDataService } from "../services/internalOrgModel.js";
 import { handleResponse } from "../utils/helpers.js";
-import matchcompaniesresponse from "../dummydata/matchcompanies.json" assert { type: "json" };
-import supplierexternaltable from "../dummydata/supplierexternaltable.json" assert { type: "json" };
-import matchgridresponse from "../dummydata/gridDataResponse.json" assert { type: "json" };
 import { updatedinsertTable, updateTable } from "../utils/db_utils.js";
 import { v4 as uuidv4 } from "uuid";
 import pool from "../config/db.js";
@@ -13,7 +10,17 @@ import axios from 'axios';
 import fs from 'fs/promises';
 
 import {OrbisGridLogin} from '../utils/auth.js';
-
+import fs from "fs";
+ 
+ const matchcompaniesresponse = JSON.parse(
+   fs.readFileSync(new URL("../dummydata/matchcompanies.json", import.meta.url))
+ );
+ const supplierexternaltable = JSON.parse(
+   fs.readFileSync(new URL("../dummydata/supplierexternaltable.json", import.meta.url))
+ );
+ const matchgridresponse = JSON.parse(
+   fs.readFileSync(new URL("../dummydata/gridDataResponse.json", import.meta.url))
+ );
 
 console.log("matchcompaniesresponse", matchcompaniesresponse);
 
